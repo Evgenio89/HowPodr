3810c5a5f2532c1c06c148856189b93856c38bed
Evgenio89 <evgenio891@gmail.com>
2022-11-07 14:00:17 +0300

    кабзда как подр. 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import {Acardion} from "./components/Acardion";
+import {Raiting, RaitingvalueType} from "./components/Raiting";
+import {OnnOff} from "./components/OnOff";
+import {AcardionUse} from "./components/AcardionUse";
+import {RaitingUse} from "./components/RaitingUse";
+import {useState} from "react";
+import {OnnOffAnControl} from "./components/OnOffAnControl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    let [reitingVelue, setReitingValue] = useState<RaitingvalueType>(2);
+    let [acardionValue, setacardionValue] = useState(true)
+    let [swichOnon, setSwichOnon] = useState(false)
+
+
+    return (
+        <div className="App">
+            <OnnOff on={swichOnon} onChange={setSwichOnon}/>
+            <OnnOffAnControl onChange={setSwichOnon}/>
+
+            <PageTitle PageTitle={"This is Appa component"}/>
+            <PageTitle PageTitle={"My friend"}/>
+            <Raiting value={reitingVelue} onClick={setReitingValue}/>
+            <AcardionUse titleValue={"Menu"}/>
+            <AcardionUse titleValue={"Users"}/>
+            <Acardion titleValue={"Menu"} colapsed={acardionValue} onChange={() => {
+                setacardionValue(!acardionValue)
+            }}/>
+            <Acardion titleValue={"Users"} colapsed={acardionValue} onChange={() => {
+                setacardionValue(!acardionValue)
+            }}/>
+            <Raiting value={reitingVelue} onClick={setReitingValue}/>
+            <RaitingUse/>
+        </div>
+    );
 }
 
-export default App;
+type PageTitlePropsType = {
+    PageTitle: string;
+};
+
+const PageTitle = (props: PageTitlePropsType) => {
+    return <h1>{props.PageTitle}</h1>;
+};
